fd4e55a8b276a4b1cc5c961949026f1aa7f1315e
Arti <arti@classplus.co>
2021-05-26 10:31:26 +0530

    Changelog: -Added files in gradle -Refactored SplashActivity.kt

--- app/src/main/java/com/chanda/lapaalap/SplashActivity.kt
+++ app/src/main/java/com/chanda/lapaalap/SplashActivity.kt
@@ -1,35 +1,26 @@
 package com.chanda.lapaalap
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.Window
-import android.view.WindowManager
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
-import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
+        goToNext()
+    }
 
-        fun goToNext() {
-                Handler().postDelayed({
-                val contentIntent = Intent(this, MainActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(contentIntent)
-                finish()
-            }, 4000)
-        }
-
-        fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_splash)
-            goToNext()
-        }
-
-
+    private fun goToNext() {
+        Handler().postDelayed({
+            val contentIntent = Intent(this@SplashActivity, MainActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(contentIntent)
+            finish()
+        }, 4000)
     }
 }
